66ae15fd2d39d323a545897ff40d82d4319c2dbe
KarimFox <KimyFuchs@hotmail.com>
2024-04-16 04:46:11 -0700

    Modal

--- src/Components/Views/Perfil/Cuenta/Cuenta.jsx
+++ src/Components/Views/Perfil/Cuenta/Cuenta.jsx
@@ -4,14 +4,15 @@ import { getCookie, eraseCookie } from "../../../../utils/cookieUtils";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AlertService from "../../../../Services/AlertService/AlertService";
 import { API_URL } from "../../../../config";
-import {  useNavigate } from "react-router-dom";
-import { Table, Container, Card, Button, Spinner, Modal } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { Table, Container, Card, Button, Spinner, Modal, Form } from "react-bootstrap";
 
 const Cuenta = () => {
   const [user, setUser] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [editedUserData, setEditedUserData] = useState(null);
   const userId = getCookie("userId");
   const navigate = useNavigate();
   const fileInputRef = useRef(null);
@@ -38,58 +39,25 @@ const Cuenta = () => {
       });
   };
 
-  const handleUpload = (file) => {
-    const formData = new FormData();
-    formData.append("file", file);
-
-    axios
-      .post(`${API_URL}/usuarios/upload/${userId}`, formData)
-      .then((response) => {
-        AlertService.success("Imagen cargada con éxito");
-        console.log("Imagen cargada con éxito:", response.data);
-        actualizarDatosUsuario();
-        setShowModal(false);
-        setSelectedFile(null);
-      })
-      .catch((error) => {
-        console.error("Error al cargar la imagen:", error);
-      });
-  };
-
-  const handleDrop = (event) => {
-    event.preventDefault();
-    const file = event.dataTransfer.files[0];
-    if (file && file.type.startsWith("image/")) {
-      setSelectedFile(file);
-    } else {
-      console.log("Por favor, sube solo archivos de imagen.");
+  const handleEditUser = () => {
+    if (editedUserData) {
+      axios.patch(`${API_URL}/usuarios/${userId}`, editedUserData)
+        .then((response) => {
+          AlertService.success("Datos de usuario actualizados con éxito");
+          console.log("Datos de usuario actualizados:", response.data);
+          actualizarDatosUsuario();
+          setShowModal(false);
+        })
+        .catch((error) => {
+          console.error("Error al actualizar los datos del usuario:", error);
+          AlertService.error("Error al actualizar los datos del usuario");
+        });
     }
   };
-
-  const handleDragOver = (event) => {
-    event.preventDefault();
-  };
-
-  const handleFileInputChange = (event) => {
-    const file = event.target.files[0];
-    if (file && file.type.startsWith("image/")) {
-      setSelectedFile(file);
-    } else {
-      console.log("Por favor, sube solo archivos de imagen.");
-    }
-  };
-
-  const handleClickFileInput = () => {
-    fileInputRef.current.click();
-  };
-
-  const handleRemoveFile = () => {
-    setSelectedFile(null);
-  };
-
+  
   const handleDeleteAccount = () => {
     axios
-      .delete(`https://clon-airbnb-api-programmingsoft.koyeb.app/usuarios/${userId}`)
+      .delete(`${API_URL}/usuarios/${userId}`)
       .then((response) => {
         AlertService.success("Cuenta eliminada con éxito");
         eraseCookie("token");
@@ -102,15 +70,20 @@ const Cuenta = () => {
         console.error("Error al eliminar la cuenta:", error);
       });
   };
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditedUserData({ ...editedUserData, [name]: value });
+  };
 
   return (
-    <div className="courses-container mt-5 " >
-          <Container style={{marginBottom: '20px'}}>
+    <div className="courses-container mt-5">
+      <Container style={{ marginBottom: '20px' }}>
         <Card className="courses-container bg-dark text-light courses-container">
-          <h1 className="mb-0 ">Perfil</h1>
+          <h1 className="mb-0">Perfil</h1>
         </Card>
-        </Container>
-      <div className="card courses-container ">
+      </Container>
+      <div className="card courses-container">
         <div className="card-body">
           {user && (
             <div className="row">
@@ -120,52 +93,40 @@ const Cuenta = () => {
                     src={user.imagePerfil.imageUrl}
                     alt="Perfil de usuario"
                     className="rounded-circle mb-4 img-fluid"
+                    style={{ marginBottom: '20px' }} // Agregamos margen inferior
                   />
                 ) : (
                   <img
                     src="https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png"
                     alt="Perfil de usuario"
                     className="rounded-circle mb-4 img-fluid"
+                    style={{ marginBottom: '20px' }} // Agregamos margen inferior
                   />
                 )}
-                <Button onClick={() => setShowModal(true)}>Subir imagen</Button>
-                <input
-                  type="file"
-                  accept="image/*"
-                  ref={fileInputRef}
-                  style={{ display: "none" }}
-                  onChange={handleFileInputChange}
-                />
-                {selectedFile && (
-                  <div>
-                    <p>Nombre del archivo: {selectedFile.name}</p>
-                    <p>Tipo de archivo: {selectedFile.type}</p>
-                    <Button onClick={handleRemoveFile}>Eliminar</Button>
-                  </div>
-                )}
-                <table className="table">
+                <Button variant="primary" onClick={() => setShowModal(true)}>Editar datos</Button> {/* Cambiamos a Button */}
+                <Table bordered>
                   <tbody>
                     <tr>
-                      <th scope="row">Nombre:</th>
+                      <th>Nombre:</th> {/* Cambiamos a th */}
                       <td>{user.User}</td>
                     </tr>
                     <tr>
-                      <th scope="row">Email:</th>
+                      <th>Email:</th> {/* Cambiamos a th */}
                       <td>{user.Mail}</td>
                     </tr>
                     <tr>
-                      <th scope="row">Tipo:</th>
+                      <th>Rol:</th> {/* Cambiamos a th */}
                       <td>{user.Rol}</td>
                     </tr>
                   </tbody>
-                </table>
+                </Table>
               </div>
               <div className="col-md-6 col-lg-8">
                 <div className="container">
                   <div className="card">
                     <h1>Configuración</h1>
-                    <Button variant="primary">Editar datos del usuario</Button>
-                    <Button variant="danger" onClick={() => setShowDeleteConfirmation(true)}>Eliminar cuenta</Button>
+                    <Button variant="primary" onClick={() => setShowModal(true)}>Editar datos del usuario</Button> {/* Cambiamos a Button */}
+                    <Button variant="danger" onClick={() => setShowDeleteConfirmation(true)}>Eliminar cuenta</Button> {/* Cambiamos a Button */}
                   </div>
                 </div>
               </div>
@@ -175,40 +136,31 @@ const Cuenta = () => {
       </div>
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>Subir imagen</Modal.Title>
+          <Modal.Title>Editar datos de usuario</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {selectedFile ? (
-            <div>
-              <p>Nombre del archivo: {selectedFile.name}</p>
-              <p>Tipo de archivo: {selectedFile.type}</p>
-              <Button onClick={handleRemoveFile}>Eliminar</Button>
-            </div>
-          ) : (
-            <div
-              onClick={handleClickFileInput}
-              onDrop={handleDrop}
-              onDragOver={handleDragOver}
-              style={{
-                border: "2px dashed #ccc",
-                padding: "20px",
-                textAlign: "center",
-                cursor: "pointer",
-              }}
-            >
-              <p>Arrastra y suelta aquí tu imagen</p>
-            </div>
-          )}
+          <Form>
+            <Form.Group controlId="formBasicUsername">
+              <Form.Label>Nombre de usuario</Form.Label>
+              <Form.Control type="text" placeholder="Enter username" name="User" defaultValue={user?.User} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" name="Mail" defaultValue={user?.Mail} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group controlId="formBasicRole">
+              <Form.Label>Rol</Form.Label>
+              <Form.Control type="text" placeholder="Enter role" name="Rol" defaultValue={user?.Rol} onChange={handleChange} />
+            </Form.Group>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={() => setShowModal(false)}>
             Cerrar
           </Button>
-          {selectedFile && (
-            <Button variant="primary" onClick={() => handleUpload(selectedFile)}>
-              Subir
-            </Button>
-          )}
+          <Button variant="primary" onClick={handleEditUser}>
+            Guardar cambios
+          </Button>
         </Modal.Footer>
       </Modal>
 
